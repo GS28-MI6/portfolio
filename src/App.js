8c86733bc5a617f3c7402eff5fc94428b1d22b74
GS28-MI6 <gasosa2811@gmail.com>
2020-12-12 16:59:26 -0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-
-import { BrowserRouter as Router, Route} from "react-router-dom";
-import Header from "./components/navbar"
-import Inicio from "./components/inicio"
-import Monitoreo from "./components/monitoreo"
-import Seguridad from "./components/seguridad"
-import VentasApp from "./components/ventasApp"
-import Ventas from "./components/ventas"
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/navbar";
+import Inicio from "./components/inicio";
+import Monitoreo from "./components/monitoreo";
+import Seguridad from "./components/seguridad";
+import VentasApp from "./components/ventasApp";
+import Ventas from "./components/ventas";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-    <div className="App m-0 p-0" style={{ height: "100vh" }}>
-      <Header /> 
-      <Route exact path="/" component={Inicio} />
-      <Route path="/monitoreo" component={Monitoreo} />
-      <Route path="/seguridad" component={Seguridad} />
-      <Route path="/ventasApp" component={VentasApp} />
-      <Route path="/ventas" component={Ventas} />
-    </div>
+      <div className="App m-0 p-0" style={{ height: "100vh" }}>
+        <Header />
+        <Route exact path="/" component={Inicio} />
+        <Route exact path="/monitoreo" component={Monitoreo} />
+        <Route exact path="/seguridad" component={Seguridad} />
+        <Route exact path="/ventasApp" component={VentasApp} />
+        <Route exact path="/ventas" component={Ventas} />
+      </div>
     </Router>
   );
 }
